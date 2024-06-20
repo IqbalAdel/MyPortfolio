9e355c06162f8ccf52e5258a9c0ac05e034f6c54
Iqbal Adel <IAdel@gmx.de>
2024-06-20 13:26:09 +0200

    finished header

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
