ace569127f0b8d1f6ab3908755ea3165602274fe
Iqbal Adel <IAdel@gmx.de>
2024-06-24 15:47:16 +0200

    finished projects component

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent, CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  
 }
--- src/app/main/portfolio/project/project.component.ts
+++ src/app/main/portfolio/project/project.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit{
+  @Input() marginRight:boolean = false;
 
+  projects = [
+    {
+    title: "JOIN",
+    tech: "Javascript | HTML | CSS | Firebase",
+    description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+    image: "join_img",
+    reverse: false,
+    },
+    {
+    title: "El Pollo Loco",
+    tech: "Javascript | HTML | CSS | ",
+    description: "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.",
+    image: "Pollo_loco",
+    reverse: true,
+    },
+
+  ]
+
+  screenWidth: number = 0;  
+  screenHeight: number = 0;  
+
+    
+  ngOnInit() {  
+    this.screenWidth = window.innerWidth;  
+    this.screenHeight = window.innerHeight; 
+  }  
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
 }
