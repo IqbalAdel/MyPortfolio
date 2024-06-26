51ea0210f8fa7638d25e995ae3dfef369852bdb6
Iqbal Adel <IAdel@gmx.de>
2024-06-26 17:59:21 +0200

    improved logic for contact formular

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  isChecked: boolean = false;
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(ngForm: NgForm){
+    if(ngForm.valid && ngForm.submitted && this.isChecked){
+      console.log(this.contactData)
+    }
+  }
 
 }
